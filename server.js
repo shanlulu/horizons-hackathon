49127d3244deecce66d957c9c169d4ae068bdf0e
amyible <amyible@gmail.com>
2017-07-14 12:20:37 -0700

    save item routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
 
 var models = require('./models/models');
 var ShelfItem = models.ShelfItem;
@@ -19,6 +20,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/public/index.html'); // For React/Redux
+});
+
+app.post('/save', function(req, res){
+	if (!req.body.name) console.log("Item name required");
+	new ShelfItem({
+		name: req.body.name,
+		category: req.body.category,
+		date: req.body.date.getTime(),
+		imageUrl: req.body.imageUrl
+	}).save(function(err){
+		console.log("Error saving to database", err);
+	})
+	.then(res.respond({success: true}))
+});
+
+app.post('/save/:shopItemId', function(req, res){
+	ShopItem.findById(req.params.shopItemId).exec()
+		.then(function(resp){
+			new ShelfItem({
+				name: resp.name,
+				category: resp.category,
+				date: resp.storage + (new Date().getTime()),
+				imageUrl: resp.imageUrl
+			}).save(function(err){
+				console.log("Error saving to database", err);
+			});
+		})
+		.then(res.respond({success: true}))
 });
 
 app.use('/api', api);
