5441ed9cb6a2d666b6f1f58843950a17eef015ee
allaunyc <alau11@binghamton.edu>
2017-07-14 16:19:42 -0700

    Adding login page

--- frontend/components/FridgeContent.js
+++ frontend/components/FridgeContent.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FoodItem from './FoodItem';
+import { Route, Link } from 'react-router-dom';
+
+class Login extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: '',
+        password: ''
+      }
+    }
+
+    handleName(event){
+    this.setState({name: event.target.value})
+}
+    handlePassword(event){
+    this.setState({password: event.target.value})
+}
+    handleSubmit(e){
+    e.preventDefault();
+    var name = this.state.name;
+    var phone = this.state.password;
+
+    this.setState({name: '', password: ''});
+    console.log(this.state);
+}
+
+    render() {
+    return (
+        <div>
+          <h1>Welcome</h1>
+          <form>
+            <input type="text" placeholder="Enter Name" value={this.state.name} onChange={(event) =>
+              this.handleName(event)}/>
+              <input type="password" placeholder="Enter Password" value={this.state.password} onChange={(event) =>
+              this.handlePassword(event)}/>
+              <Link to="/fridge">Check out fridge</Link>
+          </form>
+        </div>
+    );
+  }
+};
+
+Login.propTypes = {
+    name: PropTypes.string,
+    password: PropTypes.string
+};
+
 
 
 class FridgeContent extends React.Component {
@@ -29,6 +76,7 @@ class FridgeContent extends React.Component {
     render() {
         return (
             <div>
+              <Route exact={true} path="/fridge" component={Login}/>
                 {this.state.foodItems.map(item =>
                     (<FoodItem name={item.name}
                         expiryDate={item.expiryDate}
@@ -43,7 +91,7 @@ class FridgeContent extends React.Component {
 
 FoodItem.propTypes = {
     name: PropTypes.string,
-    expiryDate: PropTypes.object, // careful with what we receive
+    expiryDate: PropTypes.string, // careful with what we receive
     imageUrl: PropTypes.string,
     category: PropTypes.string
 };
