306f7858c3bfdfba14a2bf71e5db4eb76f039f95
amyible <amyible@gmail.com>
2017-07-14 18:15:54 -0700

    add recipe post route

--- backend/routes.js
+++ backend/routes.js
@@ -89,5 +89,36 @@ router.post('/save/:shopItemId', function(req, res){
 		.then(res.respond({success: true}))
 });
 
+router.get('/recipes', function(req, res){
+  ShelfItem.find().exec()
+    .then(function(resp){
+      var arr = resp.map((item) => (item.name));
+      return arr;
+    })
+    .then(function(arr){
+      var kitchenString = arr.join(',');
+      $.ajax({
+        url: 'http://www.supercook.com/dyn/results',
+        method: 'post',
+        data: {
+          needsimage: "1",
+          kitchen: kitchenString,
+          focus: "",
+          kw: "",
+          catname: ",",
+          exclude: "",
+          start: "0"
+        },
+        success: function(resp){
+          var results = resp.responseJSON.results;
+          res.respond({recipes: results});
+        },
+        error: function(err){
+          console.log("Failure getting recipes");
+        }
+      });
+    })
+})
+
 
 module.exports = router;
